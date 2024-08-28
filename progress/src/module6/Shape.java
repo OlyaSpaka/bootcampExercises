a00be745dfa3f26c781491a1e44744c320f2d578
Olga <shpakaolga@gmail.com>
2024-08-28 13:41:28 +0300

    "Activity 2 – Abstract Class" module6 task separated in a way that Shape is the only class in Shape.java

--- progress/src/module6/Shape.java
+++ progress/src/module6/Shape.java
@@ -18,31 +18,3 @@ public abstract class Shape {
         return color;
     }
 }
-
-
-class Circle extends Shape {
-    private double radius;
-
-    public Circle(double radius, String color) {
-        this.radius = radius;
-        setColor(color);
-    }
-
-    @Override
-    public double calculateArea() {
-        return Math.PI * radius * radius;
-    }
-
-    @Override
-    public double calculatePerimeter() {
-        return 2 * Math.PI * radius;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-}
