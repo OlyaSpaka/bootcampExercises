2395e970a2985c146f88a60a765f42c9541fe531
Olga <shpakaolga@gmail.com>
2024-08-28 12:03:32 +0300

    Formatted

--- progress/src/module4/DateActivity.java
+++ progress/src/module4/DateActivity.java
@@ -4,8 +4,7 @@ import java.util.Date;
 
 public class DateActivity {
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Date d = new Date();
         System.out.println(d);
     }
